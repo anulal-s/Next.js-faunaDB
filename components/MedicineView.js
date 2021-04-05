c3810123e4d8eef838102b1d0dc2b270bd518d46
Anulal S <anulals@gmail.com>
2021-04-05 15:54:44 +0100

    class to className

--- components/MedicineView.js
+++ components/MedicineView.js
@@ -9,21 +9,22 @@ export default function MedicineView({ medicine, dealer }) {
     
     return (
         <div>
-            <div class="bg-gray-100 p-4 rounded-md my-2 shadow-lg">
+            <div className="bg-gray-100 p-4 rounded-md my-2 shadow-lg">
                 
-                <p class="text-gray-900 mb-4">{medicine.data.description}</p>
+                <p className="text-gray-900 mb-4">{medicine.data.description}</p>
 
-                <p class="text-gray-900">{medicine.data.details}</p>
+                <p className="text-gray-900">{medicine.data.details}</p>
             </div>
             
             <h3 className="text-red-100 text-1xl mb-4 mt-4">Dealer</h3>
             
-            <div class="bg-gray-100 p-4 rounded-md my-2 shadow-lg">
-                <p class="text-gray-900 mb-4">{dealer.name}</p>
+            <div className="bg-gray-100 p-4 rounded-md my-2 shadow-lg">
+                
+                <p className="text-gray-900 mb-4">{dealer.name}</p>
 
-                <p class="text-gray-900 mb-4">{dealer.description}</p>
+                <p className="text-gray-900 mb-4">{dealer.description}</p>
 
-                <p class="text-gray-900">{dealer.details}</p>
+                <p className="text-gray-900">{dealer.details}</p>
 
             </div>
 
